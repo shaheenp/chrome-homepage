165763a9372819f21eabc4a3f4a66c8a830e6d71
kiavashp <kiavashp@gmail.com>
2016-12-25 16:01:06 -0700

    folders + styles

--- index.js
+++ index.js
@@ -1,35 +1,75 @@
+'use strict';
+
+const htmlEscape = text => {
+
+    // TODO: implement or use library
+    return text;
+};
+const htmlToNode = html => {
+    let e = document.createElement('div');
+    e.innerHTML = html;
+    return e.firstElementChild;
+};
+const shtml = (strings, ...values) => {
+    let result = [strings[0]];
+
+    values.forEach((value, i) => {
+        let escaped = htmlEscape(value || '');
+        result.push(escaped, strings[i + 1]);
+    });
+
+    let html = result.join('');
+
+    return html;
+};
+const html = (...args) => {
+    let html = shtml(...args);
+    let node = htmlToNode(html);
+
+    return node;
+};
+
+const render = {};
+
+render.links = nodes => {
+    return shtml`<ul class="links">
+        ${nodes.map(n => n.url ? `
+            <a href="${n.url}"><li class="link">${n.title || n.url}</li></a>
+        ` : `
+            <li class="folder">
+                <div class="folder-title">${n.title}</div>
+                ${render.links(n.children)}
+            </li>
+        `).join('')}
+    </ul>`;
+};
+
 chrome.bookmarks.getSubTree('1', tree => {
 
-    let folders = tree[0].children;
-    let contentElement = document.getElementById('content');
+    let nodes = tree[0].children;
+    let content = document.getElementById('content');
 
-    for (let folder of folders) {
-
-        // all top level folders should be folders w/ items
-        if (Array.isArray(folder.children) && folder.children.length) {
-
-            let bookmarks = folder.children;
-            let sectionElement = contentElement.appendChild(document.createElement('div'));
-            sectionElement.className = 'section';
-
-            let sectionTitleElement = sectionElement.appendChild(document.createElement('div'));
-            sectionTitleElement.className = 'title';
-            sectionTitleElement.textContent = folder.title;
-
-            let sectionLinksElement = sectionElement.appendChild(document.createElement('div'));
-            sectionLinksElement.className = 'links';
-
-            for (let bookmark of bookmarks) {
-                let bookmarkLinkElement = sectionLinksElement.appendChild(document.createElement('a'));
-                bookmarkLinkElement.href = bookmark.url;
-                let bookmarkElement = bookmarkLinkElement.appendChild(document.createElement('p'));
-                bookmarkElement.textContent = bookmark.title;
-            }
-
-        } else {
-            console.log('found folder that either wasn\'t folder or was empty folders');
-        }
-
+    for (let node of nodes) {
+        content.appendChild(html`
+            <section>
+                <div class="title">${node.children && node.title}</div>
+                ${render.links(node.children || [node])}
+            </section>
+        `);
     }
+});
 
+document.addEventListener('DOMContentLoaded', function () {
+    document.body.addEventListener('click', event => {
+        let target = event.target;
+
+        while (target) {
+            if (target && target.classList && target.classList.contains('folder')) {
+                target.classList.toggle('open');
+                break;
+            } else {
+                target = target.parentNode;
+            }
+        }
+    });
 });
