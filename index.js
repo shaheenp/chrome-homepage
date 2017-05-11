036ee56979c4429bfe414dc01224a46a581a1348
shaheenp <shaheenspage@gmail.com>
2017-05-10 18:41:46 -0600

    refactored code to be more organized

--- index.js
+++ index.js
@@ -1,38 +1,53 @@
+'use strict';
+
+function htmlStringToNode (htmlString) {
+    let tempElement = document.createElement('div');
+    tempElement.innerHTML = htmlString;
+
+    return tempElement.firstElementChild;
+}
+
+function stringHtml (strings, ...values) {
+    let result = [strings[0]];
+
+    values.forEach((value, v) => {
+        result.push(value, strings[v + 1]);
+    });
+
+    return result.join('');
+}
+
+function toHtml (...args) {
+    let html = stringHtml(...args);
+    let htmlNode = htmlStringToNode(html);
+
+    return htmlNode;
+}
+
+function renderLinks (bookmarks) {
+    return stringHtml`<div class="links">
+        ${bookmarks.map(bookmark => `<a href="${bookmark.url}"><p>${bookmark.title}</p></a>`).join('')}
+    </div>`;
+}
+
 chrome.bookmarks.getSubTree('1', tree => {
 
-    let folders = tree[0].children;
+    let bookmarks = tree[0].children;
     let contentElement = document.getElementById('content');
 
-    for (let folder of folders) {
+    for (let bookmark of bookmarks) {
 
-        // don't display folders that begin with a "."
-        if (folder.title.length && folder.title[0] === '.') {
+        // don't display bookmarks that begin with a "."
+        if (bookmark.title[0] === '.') {
             continue;
         }
 
-        // all top level items should be bookmark folders
-        if (Array.isArray(folder.children) && folder.children.length) {
-
-            let bookmarks = folder.children;
-            let sectionElement = contentElement.appendChild(document.createElement('div'));
-            sectionElement.className = 'section';
-
-            let sectionTitleElement = sectionElement.appendChild(document.createElement('div'));
-            sectionTitleElement.className = 'title';
-            sectionTitleElement.textContent = folder.title;
-
-            let sectionLinksElement = sectionElement.appendChild(document.createElement('div'));
-            sectionLinksElement.className = 'links';
-
-            for (let bookmark of bookmarks) {
-                let bookmarkLinkElement = sectionLinksElement.appendChild(document.createElement('a'));
-                bookmarkLinkElement.href = bookmark.url;
-                let bookmarkElement = bookmarkLinkElement.appendChild(document.createElement('p'));
-                bookmarkElement.textContent = bookmark.title;
-            }
-
-        } else {
-            console.log('found folder that either wasn\'t folder or was empty folders');
+        // all top level bookmarks should be folders
+        if (Array.isArray(bookmark.children) && bookmark.children.length) {
+            contentElement.appendChild(toHtml`<div class="section">
+                <div class="title">${bookmark.title}</div>
+                ${renderLinks(bookmark.children)}
+            </div>`);
         }
 
     }
